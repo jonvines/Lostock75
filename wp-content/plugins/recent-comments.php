94f2b5788ac663feb835ccf2bfd94216795b82d8
Jon Vines <jon.vines@gmail.com>
2013-09-26 17:59:11 +0100

    Comments and construction widget

---
+++ wp-content/plugins/recent-comments.php
@@ -0,0 +1,251 @@
+<?php
+/*
+Plugin Name: Recent Comments custom
+Plugin URI: TODO
+Description: This plugin displays recent comments
+Version: 1.0
+Author: Jonathan Vines
+Author URI: TODO
+Author Email: TODO
+Text Domain: recent-comments-locale
+Domain Path: /lang/
+Network: false
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Copyright 2013 TODO (jonvines@gmail.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// TODO: change 'My_Recent_Posts' to the name of your plugin
+class My_Recent_Comments_Custom extends WP_Widget {
+
+	/*--------------------------------------------------*/
+	/* Constructor
+	/*--------------------------------------------------*/
+
+	/**
+	 * Specifies the classname and description, instantiates the widget,
+	 * loads localization files, and includes necessary stylesheets and JavaScript.
+	 */
+	public function __construct() {
+
+		// load plugin text domain
+		add_action( 'init', array( $this, 'widget_textdomain' ) );
+
+		// Hooks fired when the Widget is activated and deactivated
+		register_activation_hook( __FILE__, array( $this, 'activate' ) );
+		register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
+
+		// TODO:	update classname and description
+		// TODO:	replace 'recent-comments-locale' to be named more plugin specific. Other instances exist throughout the code, too.
+		parent::__construct(
+			'recent-comments-id',
+			__( 'Recent comments', 'recent-comments-locale' ),
+			array(
+				'classname'		=>	'recent-comments-widget',
+				'description'	=>	__( 'Recent comments', 'recent-comments-locale' )
+			)
+		);
+
+		// Register admin styles and scripts
+		//add_action( 'admin_print_styles', array( $this, 'register_admin_styles' ) );
+		//add_action( 'admin_enqueue_scripts', array( $this, 'register_admin_scripts' ) );
+
+		// Register site styles and scripts
+		//add_action( 'wp_enqueue_scripts', array( $this, 'register_widget_styles' ) );
+		//add_action( 'wp_enqueue_scripts', array( $this, 'register_widget_scripts' ) );
+
+	} // end constructor
+
+	/*--------------------------------------------------*/
+	/* Widget API Functions
+	/*--------------------------------------------------*/
+
+	/**
+	 * Outputs the content of the widget.
+	 *
+	 * @param	array	args		The array of form elements
+	 * @param	array	instance	The current instance of the widget
+	 */
+	public function widget( $args, $instance ) {
+global $comments, $comment;
+
+		$cache = wp_cache_get('widget_recent_comments_custom', 'widget');
+
+		if ( ! is_array( $cache ) )
+			$cache = array();
+
+		if ( ! isset( $args['widget_id'] ) )
+			$args['widget_id'] = $this->id;
+
+		if ( isset( $cache[ $args['widget_id'] ] ) ) {
+			echo $cache[ $args['widget_id'] ];
+			return;
+		}
+
+ 		extract($args, EXTR_SKIP);
+ 		$output = '';
+
+		$title = ( ! empty( $instance['title'] ) ) ? $instance['title'] : __( 'Recent Comments' );
+		$title = apply_filters( 'widget_title', $title, $instance, $this->id_base );
+		$number = ( ! empty( $instance['number'] ) ) ? absint( $instance['number'] ) : 5;
+		if ( ! $number )
+ 			$number = 5;
+
+		$comments = get_comments( apply_filters( 'widget_comments_args', array( 'number' => $number, 'status' => 'approve', 'post_status' => 'publish' ) ) );
+		$output .= $before_widget;
+		if ( $title )
+			$output .= $before_title . $title . $after_title;
+
+		$output .= '<ul id="recentcomments">';
+		if ( $comments ) {
+			// Prime cache for associated posts. (Prime post term cache if we need it for permalinks.)
+			$post_ids = array_unique( wp_list_pluck( $comments, 'comment_post_ID' ) );
+			_prime_post_caches( $post_ids, strpos( get_option( 'permalink_structure' ), '%category%' ), false );
+
+			foreach ( (array) $comments as $comment) {
+				$output .=  '<li class="recentcomments">' . get_comment_text($comment->comment_ID) . '</li>';
+			}
+ 		}
+		$output .= '</ul>';
+		$output .= $after_widget;
+
+		echo $output;
+		$cache[$args['widget_id']] = $output;
+		wp_cache_set('widget_recent_comments_custom', $cache, 'widget');
+	} // end widget
+
+    function flush_widget_cache() {
+		wp_cache_delete('widget_recent_posts_custom', 'widget');
+	}
+
+	/**
+	 * Processes the widget's options to be saved.
+	 *
+	 * @param	array	new_instance	The new instance of values to be generated via the update.
+	 * @param	array	old_instance	The previous instance of values before the update.
+	 */
+	public function update( $new_instance, $old_instance ) {
+        $instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['number'] = absint( $new_instance['number'] );
+		$this->flush_widget_cache();
+
+		$alloptions = wp_cache_get( 'alloptions', 'options' );
+		if ( isset($alloptions['widget_recent_comments']) )
+			delete_option('widget_recent_comments');
+
+		return $instance;
+
+	} // end widget
+
+	/**
+	 * Generates the administration form for the widget.
+	 *
+	 * @param	array	instance	The array of keys and values for the widget.
+	 */
+	public function form( $instance ) {
+        $title  = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
+		$number = isset( $instance['number'] ) ? absint( $instance['number'] ) : 5;
+?>
+		<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" /></p>
+
+		<p><label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php _e( 'Number of comments to show:' ); ?></label>
+		<input id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" type="text" value="<?php echo $number; ?>" size="3" /></p>
+<?php
+
+		// Display the admin form
+		include( plugin_dir_path(__FILE__) . '/views/admin.php' );
+
+	} // end form
+
+	/*--------------------------------------------------*/
+	/* Public Functions
+	/*--------------------------------------------------*/
+
+	/**
+	 * Loads the Widget's text domain for localization and translation.
+	 */
+	public function widget_textdomain() {
+
+		// TODO be sure to change 'recent-comments-excerpt' to the name of *your* plugin
+		load_plugin_textdomain( 'recent-comments-locale', false, plugin_dir_path( __FILE__ ) . '/lang/' );
+
+	} // end widget_textdomain
+
+	/**
+	 * Fired when the plugin is activated.
+	 *
+	 * @param		boolean	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog.
+	 */
+	public function activate( $network_wide ) {
+		// TODO define activation functionality here
+	} // end activate
+
+	/**
+	 * Fired when the plugin is deactivated.
+	 *
+	 * @param	boolean	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog
+	 */
+	public function deactivate( $network_wide ) {
+		// TODO define deactivation functionality here
+	} // end deactivate
+
+	/**
+	 * Registers and enqueues admin-specific styles.
+	 */
+	public function register_admin_styles() {
+
+		// TODO:	Change 'recent-comments-excerpt' to the name of your plugin
+		wp_enqueue_style( 'recent-comments-admin-styles', plugins_url( 'recent-comments-excerpt/css/admin.css' ) );
+
+	} // end register_admin_styles
+
+	/**
+	 * Registers and enqueues admin-specific JavaScript.
+	 */
+	public function register_admin_scripts() {
+
+		// TODO:	Change 'recent-comments-excerpt' to the name of your plugin
+		wp_enqueue_script( 'recent-comments-admin-script', plugins_url( 'recent-comments-excerpt/js/admin.js' ), array('jquery') );
+
+	} // end register_admin_scripts
+
+	/**
+	 * Registers and enqueues widget-specific styles.
+	 */
+	public function register_widget_styles() {
+
+		// TODO:	Change 'recent-comments-excerpt' to the name of your plugin
+		wp_enqueue_style( 'recent-comments-widget-styles', plugins_url( 'recent-comments-excerpt/css/widget.css' ) );
+
+	} // end register_widget_styles
+
+	/**
+	 * Registers and enqueues widget-specific scripts.
+	 */
+	public function register_widget_scripts() {
+
+		// TODO:	Change 'recent-comments-excerpt' to the name of your plugin
+		wp_enqueue_script( 'recent-comments-script', plugins_url( 'recent-comments-excerpt/js/widget.js' ), array('jquery') );
+
+	} // end register_widget_scripts
+
+} // end class
+
+// TODO:	Remember to change 'My_Recent_Posts' to match the class name definition
+add_action( 'widgets_init', create_function( '', 'register_widget("My_Recent_Comments_Custom");' ) );
